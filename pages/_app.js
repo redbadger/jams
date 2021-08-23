caef3ba2b662cd11d11b599e06b02744e02afc47
Siame Rafiq <me@sia.me>
2021-08-23 16:00:36 +0100

    Add Chakra UI and basic JamButton usage

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css';
 import { CookiesProvider } from 'react-cookie';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <CookiesProvider>
-      <Component {...pageProps} />
-    </CookiesProvider>
+    <ChakraProvider>
+      <CookiesProvider>
+        <Component {...pageProps} />
+      </CookiesProvider>
+    </ChakraProvider>
   );
 }
 
