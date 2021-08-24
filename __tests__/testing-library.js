a0dd98bc73f551d96fe24d5cda7a43e66487f39f
Monika Ferencz-Szabo <midnitem@gmail.com>
2021-08-24 15:34:19 +0100

    Get rid of next-auth error when running tests

--- __tests__/testing-library.js
+++ __tests__/testing-library.js
@@ -1,18 +1,31 @@
 /**
  * @jest-environment jsdom
  */
-import React from 'react'
-import { render } from '@testing-library/react'
-import Index from '../pages/index'
+import React from 'react';
+import { render } from '@testing-library/react';
+import Index from '../pages/index';
+import client from 'next-auth/client';
+import '@testing-library/jest-dom';
+
+jest.mock('next-auth/client');
+
+beforeAll(() => {
+  const mockSession = {
+    expires: '1',
+    user: { email: 'jam', name: 'jam' },
+  };
+
+  client.useSession.mockReturnValueOnce([mockSession, false]);
+});
 
 describe('App', () => {
   it('renders a heading', () => {
-    const { getByRole } = render(<Index />)
+    const { getByRole } = render(<Index />);
 
     const heading = getByRole('heading', {
       name: /welcome to jam/i,
-    })
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(heading).toBeInTheDocument();
+  });
+});
