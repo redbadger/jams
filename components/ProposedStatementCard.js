98af45b8a178ef934afc087f07fafee2477e3f79
Anubha Singh <45950125+anubha22@users.noreply.github.com>
2021-08-25 14:48:47 +0100

    Adding visible editable components.

--- components/ProposedStatementCard.js
+++ components/ProposedStatementCard.js
@@ -5,10 +5,37 @@ import {
   Text,
   Button,
   Flex,
+  Textarea,
 } from '@chakra-ui/react';
 import { Fragment } from 'react';
+import { useState } from 'react';
 
-function ProposedStatementCard({ children, index, onClick }) {
+const EditableStatement = ({
+  children,
+  index,
+  onClick,
+  onCancel,
+}) => {
+  return (
+    <>
+      <VStack>
+        <Textarea size="md" defaultValue={children} />
+      </VStack>
+      <Flex justifyContent="flex-end">
+        <Button onClick={() => onCancel()}>Cancel</Button>
+        <Button>Save</Button>
+      </Flex>
+    </>
+  );
+};
+
+// Add visible only component.
+const VisibleOnlyStatement = ({
+  children,
+  index,
+  onClick,
+  onEdit,
+}) => {
   return (
     <>
       <VStack>
@@ -16,10 +43,42 @@ function ProposedStatementCard({ children, index, onClick }) {
       </VStack>
       <Flex justifyContent="flex-end">
         <Button onClick={() => onClick(index)}>Delete</Button>
-        <Button>Edit</Button>
+        <Button onClick={() => onEdit()}>Edit</Button>
       </Flex>
     </>
   );
+};
+
+function ProposedStatementCard(props) {
+  const [isEditable, setIsEditable] = useState(false);
+
+  const handleEdit = () => {
+    setIsEditable((isEditable) => !isEditable);
+    console.log('edit clicked');
+  };
+
+  // show component based on whether editing or viewing only.
+  if (isEditable) {
+    return (
+      <EditableStatement
+        index={props.index}
+        onClick={props.onClick}
+        onCancel={handleEdit}
+      >
+        {props.children}
+      </EditableStatement>
+    );
+  } else {
+    return (
+      <VisibleOnlyStatement
+        index={props.index}
+        onClick={props.onClick}
+        onEdit={handleEdit}
+      >
+        {props.children}
+      </VisibleOnlyStatement>
+    );
+  }
 }
 
 export default ProposedStatementCard;
