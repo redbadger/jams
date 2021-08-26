4c3ee969be26cc52758bf6b09b61e19588b3c9da
Rui Ramos <ruiramos@gmail.com>
2021-08-26 12:09:04 +0100

    Move admin middleware into utils file

--- pages/api/new-function.js
+++ pages/api/new-function.js
@@ -1,44 +1,4 @@
-import Cookies from 'cookies';
-import jwt from 'jsonwebtoken';
-import jwksClient from 'jwks-rsa';
-
-const client = jwksClient({
-  jwksUri: 'https://rb-jams-dev.eu.auth0.com/.well-known/jwks.json',
-});
-
-function getKey(header, callback) {
-  client.getSigningKey(header.kid, function (err, key) {
-    var signingKey = key.publicKey || key.rsaPublicKey;
-    callback(null, signingKey);
-  });
-}
-
-function ensureAdmin(req, res) {
-  return new Promise((resolve, reject) => {
-    let cookies = new Cookies(req, res);
-    let token = cookies.get('next-auth.session-token');
-
-    if (!token) return reject({ message: 'No session found' });
-
-    let decodedToken = jwt.decode(token);
-
-    let accessToken = decodedToken.accessToken;
-
-    jwt.verify(
-      accessToken,
-      getKey,
-      {
-        audience: 'https://functions.jams.com',
-      },
-      function (err, decoded) {
-        if (err) {
-          reject(err);
-        }
-        resolve(decoded);
-      },
-    );
-  });
-}
+import ensureAdmin from 'utils/admin-auth-middleware.js';
 
 export default async function handler(req, res) {
   try {
