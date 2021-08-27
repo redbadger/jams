8a21db8f7b77d57c799f72711b1346c0c6933446
Siame Rafiq <me@sia.me>
2021-08-27 18:46:39 +0100

    Add PATCH to /statement and add approve/reject

--- pages/api/statement.js
+++ pages/api/statement.js
@@ -1,11 +1,6 @@
 import fire from '../../config/firebaseAdminConfig';
 
-export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${method} Not Allowed`);
-  }
-
+const handlePost = (req, res) => {
   const { jamId, statement } = req.body;
   const db = fire.firestore();
   const jamsRef = db.collection('jams');
@@ -30,4 +25,38 @@ export default function handler(req, res) {
         console.error('Error writing document: ', error);
       });
   });
+};
+
+const handlePatch = (req, res) => {
+  const { jamId, statementId, ...body } = req.body;
+  const db = fire.firestore();
+  const jamsRef = db.collection('jams');
+
+  return new Promise(() => {
+    jamsRef
+      .doc(jamId)
+      .collection('statements')
+      .doc(statementId)
+      .update(body)
+      .then(() => {
+        res.status(200).end();
+      })
+      .catch((error) => {
+        console.error('Error writing document: ', error);
+      });
+  });
+};
+
+export default function handler(req, res) {
+  if (!['POST', 'PATCH'].includes(req.method)) {
+    res.setHeader('Allow', ['POST', 'PATCH']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return;
+  }
+
+  if (req.method == 'POST') {
+    return handlePost(req, res);
+  } else if (req.method == 'PATCH') {
+    return handlePatch(req, res);
+  }
 }
