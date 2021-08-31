e6f371d018b78a76a76e3ff3cd599e5d450e6f33
Rui Ramos <ruiramos@gmail.com>
2021-08-31 12:37:43 +0100

    Inspect NODE_ENV in production

--- pages/api/new-function.js
+++ pages/api/new-function.js
@@ -1,6 +1,8 @@
 import ensureAdmin from 'utils/admin-auth-middleware.js';
 
 export default async function handler(req, res) {
+  console.log(process.env);
+  console.log(process.env.NODE_ENV);
   try {
     const token = await ensureAdmin(req, res);
 
