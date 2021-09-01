39c18d4db8ad56e5c4a2965bfcc9ed865eda6c59
Anubha Singh <45950125+anubha22@users.noreply.github.com>
2021-09-01 11:51:29 +0100

    Changing loading page.

---
+++ pages/dataexport/index.js
@@ -0,0 +1,21 @@
+import { Container, Heading, Button } from '@chakra-ui/react';
+
+function CSVObject() {
+  const jamId = '8vXobX0QDTo7cliXVtx5';
+
+  const handleCsv = () => {
+    console.log(`Calling the api with this jamId: ${jamId}`);
+    window.location = `/api/exporting/data-csv?jamId=${jamId}`;
+  };
+
+  return (
+    <Container>
+      <Heading mt="25px">CSV Objects</Heading>
+      <Button mt="150px" onClick={() => handleCsv()}>
+        Export Data
+      </Button>
+    </Container>
+  );
+}
+
+export default CSVObject;
