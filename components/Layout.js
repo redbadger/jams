5ee65e5c12c83fda1282b901fa8530aacd7f2cf1
Siame Rafiq <me@sia.me>
2021-09-06 15:53:45 +0100

    Fix grid spacing on moderator home page

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import { Container, SimpleGrid } from '@chakra-ui/react';
 export default function Layout({ children, ...props }) {
   return (
     <Container maxW={'container.lg'} {...props}>
-      <SimpleGrid columns={{ sm: 1, md: 6 }} spacing={6} spacingY={0}>
+      <SimpleGrid columns={{ sm: 1, md: 6 }} spacing={6}>
         {children}
       </SimpleGrid>
     </Container>
